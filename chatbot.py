58c354d0b06badcd70411c6d098289ed6de590f9
pankaj kumar pandit <pkumarpandit25@gmail.com>
2021-12-06 15:12:16 +0530

    3rd commit

--- chatbot.py
+++ chatbot.py
@@ -6,22 +6,6 @@ from chatterbot.trainers import ChatterBotCorpusTrainer
 
 chatbot = ChatBot('ChatBot')
 
-# chatbot = ChatBot(
-#     'ChatBot',
-#     storage_adapter='chatterbot.storage.SQLStorageAdapter',
-#     logic_adapters=[
-#         'chatterbot.logic.MathematicalEvaluation',
-#         'chatterbot.logic.TimeLogicAdapter',
-#         'chatterbot.logic.BestMatch',
-#         {
-#             'import_path': 'chatterbot.logic.BestMatch',
-#             'default_response': 'I am sorry, but I do not understand. I am still learning.',
-#             'maximum_similarity_threshold': 0.90
-#         }
-#     ],
-#     database_uri='sqlite:///database.sqlite3'
-# ) 
-
  # Training with Personal Ques & Ans 
 conversation = [
     "Hey",
@@ -37,10 +21,8 @@ conversation = [
     "You're welcome.",
     "bye",
     "Ok bye, thank you",
-    "I love you",
-    "I love you to",
     "do you know me",
-    "yes, you are person",
+    "yes, you are a humen",
     "You've Dinner",
     "I have internet, I don't need, Thank you",
     "Have you done"
